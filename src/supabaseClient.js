4d1d68f4ebdd953f799391f0f4419a6f0e6635db
RoxieC44 <r.denis44770@gmail.com>
2026-08-18 00:28:22 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'TON_URL_SUPABASE'; // Ex: https://xyz.supabase.co
-const SUPABASE_ANON_KEY = 'TA_CLE_ANON_SUPABASE'; // Ta longue clé publique
+const SUPABASE_URL = 'https://zlmnzbavrbslogdtewwt.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_pNB0R83QxSu3g7-BwWOXXw_Y9X8Xaqg';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
