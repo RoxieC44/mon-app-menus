4a1442d8df67ac0cf0c19c8a7558125538a948f8
RoxieC44 <r.denis44770@gmail.com>
2026-09-02 21:39:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,15 @@ const DEFAULT_RECIPES = [
 ];
 
 const INITIAL_EQUIPMENTS = ['Thermomix', 'Cookeo', 'Poêle', 'Four', 'Casserole', 'Airfryer', 'Gaufrier - Croque-Monsieur - Panini', 'Crêpière - Mini woks - Grill', 'Raclette - Pierrade - Fondue', 'Plancha', 'Barbecue', 'Sans Cuisson'];
-const CARBS = ['Pâtes', 'Pommes de terre', 'Semoule', 'Riz', 'Blé', 'Plaisir', 'Autre'];
+const CARBS = [
+  'Blé',
+  'Pâtes',
+  'Riz',
+  'Pommes de terre',
+  'Semoule',
+  'Plaisir',
+  'Autre'
+];
 const SEASONS_LIST = ['Printemps', 'Été', 'Automne', 'Hiver'];
 const STORAGE_ZONES = ['Placard', 'Frigo', 'Congélateur'];
 
